8647b390c2261ec1e3488e5aa56f0f53718711dd
anderson <anderson@ABL-PC>
2014-12-29 00:23:43 -0200

    .

---
+++ agenda-portlet/docroot/WEB-INF/src/br/com/abware/agenda/CalendarModel.java
@@ -0,0 +1,47 @@
+package br.com.abware.agenda;
+
+import java.util.Date;
+
+import org.primefaces.model.DefaultScheduleEvent;
+import org.primefaces.model.LazyScheduleModel;
+
+import br.com.abware.agenda.bean.ScheduleBean;
+
+public class CalendarModel extends LazyScheduleModel {
+
+	private static final long serialVersionUID = 1L;
+	
+	private RoomModel roomModel;
+	 
+	public CalendarModel(RoomModel roomModel) {
+		super();
+		this.roomModel = roomModel;
+	}
+	
+	@Override
+    public void loadEvents(Date start, Date end) {
+    	clear();
+    	BookingModel model = new BookingModel();
+		for (BookingModel b : model.getBookings(roomModel, start, end)) {
+			try {
+				addEvent(createEvent(b));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public RoomModel getRoomModel() {
+		return roomModel;
+	}
+	
+	public DefaultScheduleEvent createEvent(BookingModel booking) throws Exception {
+		DefaultScheduleEvent event; 
+		event = new DefaultScheduleEvent(ScheduleBean.getUserFlatName(booking.getUser()), 
+										 booking.getDate(), 
+										 booking.getDate(), 
+										 ScheduleBean.getRoomStyleClass(booking.getRoom()));
+		event.setData(booking);
+		return event;
+	}
+}
