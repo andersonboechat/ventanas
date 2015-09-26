e3c18bdfb38042ed567435ea7ca7b89d8375e9ed
anderson <anderson@ABL-PC>
2015-09-26 10:55:25 -0300

    .

--- jcondo-commons/src/br/com/atilo/jcondo/commons/faces/SelectItemsConverter.java
+++ jcondo-commons/src/br/com/atilo/jcondo/commons/faces/SelectItemsConverter.java
@@ -13,14 +13,14 @@ import org.apache.commons.collections.CollectionUtils;
 import br.com.abware.jcondo.core.model.BaseModel;
 import br.com.atilo.jcondo.commons.collections.IdPredicate;
 
-@FacesConverter("selectItemsConverter")
+@FacesConverter("selectItemsConverter2")
 public class SelectItemsConverter implements Converter {
 
 	@Override
 	@SuppressWarnings("unchecked")
 	public Object getAsObject(FacesContext facescontext, UIComponent uicomponent, String s) {
 		if (s != null && !s.equals("0")) {
-			List<BaseModel> models = (List<BaseModel>) ((UISelectItems) uicomponent.getChildren().get(1)).getValue();
+			List<BaseModel> models = (List<BaseModel>) ((UISelectItems) uicomponent.getChildren().get(0)).getValue();
 			return CollectionUtils.find(models, new IdPredicate(Long.parseLong(s)));
 		}
 
